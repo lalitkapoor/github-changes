fadf9a0eda200ff3aa3b223d4578a8f4420b70b8
Lalit Kapoor <lalitkapoor@gmail.com>
2014-11-15 15:19:45 -0500

    release script

---
+++ release.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+set -u
+set -o pipefail
+
+npm version patch
+VERSION=`cat package.json | json version`
+node bin/index.js -o lalitkapoor -r github-changes --only-pulls -v -a --use-commit-body --reverse-changes -n v$VERSION
+git commit --amend --no-edit
+git push origin master
+npm publish
