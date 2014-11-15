c990e4a226206aeebca73b6d49559d4e94b61fe6
Lalit Kapoor <lalitkapoor@gmail.com>
2014-11-15 15:23:26 -0500

    ensure we are on the master branch

--- release.sh
+++ release.sh
@@ -4,6 +4,7 @@ set -e
 set -u
 set -o pipefail
 
+git checkout master
 npm version patch
 VERSION=`cat package.json | json version`
 node bin/index.js -o lalitkapoor -r github-changes --only-pulls -v -a --use-commit-body --reverse-changes -n v$VERSION
