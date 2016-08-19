8dbd12e99c5cd690cfcc30381428d79b661fe6ad
Lalit Kapoor <lalitkapoor@gmail.com>
2016-08-19 04:42:28 -0400

    improvement to release script
    npm install any missing dependencies

--- release.sh
+++ release.sh
@@ -5,6 +5,7 @@ set -u
 set -o pipefail
 
 git checkout master
+npm install
 npm version patch
 VERSION=`cat package.json | json version`
 node bin/index.js -o lalitkapoor -r github-changes --only-pulls -v -a --use-commit-body --reverse-changes -n v$VERSION
