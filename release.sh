5e9d166fb65101f7c3707dd36ece7ef385a7f40b
Lalit Kapoor <lalitkapoor@gmail.com>
2017-03-16 10:23:55 -0400

    update release command to take in semver change

--- release.sh
+++ release.sh
@@ -4,9 +4,21 @@ set -e
 set -u
 set -o pipefail
 
+if [ $# -ne 1 ]
+  then
+    echo "must specify a semver value (major, minor, patch)"
+    exit
+fi
+
+if ([ "$1" != "major" ] && [ "$1" != "minor" ] && [ "$1" != "patch" ])
+  then
+    echo "please specify one of (major, minor, patch)"
+    exit
+fi
+
 git checkout master
 npm install
-npm version patch
+npm version $1
 VERSION=`cat package.json | json version`
 node bin/index.js -o lalitkapoor -r github-changes --only-pulls -v -a --use-commit-body --reverse-changes -n v$VERSION
 git add CHANGELOG.md
